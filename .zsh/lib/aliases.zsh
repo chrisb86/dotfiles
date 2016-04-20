9ddde607d0f396b14452f4e1db89ba498a1f6883
Christian Busch <chris@debilux.org>
2016-04-20 10:52:38 +0200

    Added an alias for changing terminal tab name and title.

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -8,6 +8,7 @@ alias nmap_localnet='nmap -sP 192.168.1.0/24'
 alias sulast='sudo $(history -p !-1)'
 alias whois="whois -h whois-servers.net"
 alias ltmux="if tmux has-session -t $USER; then tmux attach -t $USER; else tmux new -s $USER; fi"
+alias ltitle='echo -ne "\033]0;$HOST\007"'
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
 alias r='open -a'
 alias htop='sudo htop'
