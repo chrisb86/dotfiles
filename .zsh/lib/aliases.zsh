736ca22c613d3ff3e62b8d20772a6278292a99a0
Christian Baer <chris@debilux.org>
2019-04-19 23:54:46 +0200

    Added ZSH alias duf.

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -4,6 +4,7 @@ alias ...='cd ../..'
 alias ydl="youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'"
 alias google='ping -c 10240000 google.com'
 alias foldersize='du -sh'
+alias duf='du -sk * | sort -n | while read size fname; do for unit in k M G T P E Z Y; do if [ $size -lt 1024 ]; then echo -e "${size}${unit}\t${fname}"; break; fi; size=$((size/1024)); done; done'
 alias nmap_localnet='nmap -sP 10.0.3.0/24'
 alias whois="whois -h whois-servers.net"
 alias ltmux="if tmux has-session -t $USER; then tmux attach -d -t $USER; else tmux new -s $USER; fi"
