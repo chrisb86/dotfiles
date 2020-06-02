b64f1a5c8307dc13048f527f70266954b1ef7e89
Christian Baer <chris@debilux.org>
2020-06-02 23:19:46 +0200

    Modified LSCOLORS

--- .zsh/lib/30-colors.zsh
+++ .zsh/lib/30-colors.zsh
@@ -1,6 +1,6 @@
 typeset -Ag FX FG BG
 
-LSCOLORS="gxfXcxdxbxegedabagacad"
+LSCOLORS="gxfxcxdxbxegedabagacad"
 LS_COLORS="di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 CLICOLOR="YES"
 
