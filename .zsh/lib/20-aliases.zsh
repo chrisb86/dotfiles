e220997bc59009353abb56719047f91076c5f639
Christian Baer <chris@debilux.org>
2020-06-29 23:19:36 +0200

    Updated htop alias in zsh config

--- .zsh/lib/20-aliases.zsh
+++ .zsh/lib/20-aliases.zsh
@@ -1,7 +1,6 @@
 _exists() { (( $+commands[$1] )) }
 
 _exists less			&& export PAGER=less
-_exists sudo			&& alias doas='sudo'
 
 if _exists vim; then
 		export EDITOR=vim
@@ -13,12 +12,10 @@ unfunction _exists
 alias ls='ls --color=always -h'
 alias ll='ls -lah'
 alias mkdir='mkdir -p'
-alias htop='doas htop'
 alias ...='cd ../..'
 alias foldersize='du -sh'
 alias duf='du -sk * | sort -n | while read size fname; do for unit in k M G T P E Z Y; do if [ $size -lt 1024 ]; then echo -e "${size}${unit}\t${fname}"; break; fi; size=$((size/1024)); done; done'
 alias google='ping -c 10240000 google.com'
-alias sulast='doas $(history -p !-1)'
 
 case `uname` in
   Darwin)
@@ -29,6 +26,8 @@ case `uname` in
     alias ydl="youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'"
     alias nmap_localnet='nmap -sP 10.0.3.0/24'
 		alias fetch='curl -O'
+		alias htop='sudo htop'
+		alias sulast='sudo $(history -p !-1)'
   ;;
   Linux)
     # commands for Linux go here
@@ -36,5 +35,6 @@ case `uname` in
   FreeBSD)
     # commands for FreeBSD go here
     alias stl="sockstat -l"
+		alias sulast='doas $(history -p !-1)'
   ;;
 esac
