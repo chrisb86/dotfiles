416ed81ceb48e779d306d88831fa85dfdc2539e8
Christian Baer <chris@debilux.org>
2020-10-11 16:06:46 +0200

    Refactored 20-aliases.zsh and added alias vor "history -i"

--- .zsh/lib/20-aliases.zsh
+++ .zsh/lib/20-aliases.zsh
@@ -9,25 +9,26 @@ fi
 
 unfunction _exists
 
-alias ls='ls --color=always -h'
-alias ll='ls -lah'
-alias mkdir='mkdir -p'
-alias ...='cd ../..'
-alias foldersize='du -sh'
-alias duf='du -sk * | sort -n | while read size fname; do for unit in k M G T P E Z Y; do if [ $size -lt 1024 ]; then echo -e "${size}${unit}\t${fname}"; break; fi; size=$((size/1024)); done; done'
-alias google='ping -c 10240000 google.com'
+alias ls="ls --color=always -h"
+alias ll="ls -lah"
+alias mkdir="mkdir -p"
+alias ...="cd ../.."
+alias foldersize="du -sh"
+alias duf="du -sk * | sort -n | while read size fname; do for unit in k M G T P E Z Y; do if [ $size -lt 1024 ]; then echo -e "${size}${unit}\t${fname}"; break; fi; size=$((size/1024)); done; done"
+alias google="ping -c 10240000 google.com"
+alias history="history -i"
 
 case `uname` in
   Darwin)
     # commands for OS X go here
-    alias flushdns='dscacheutil -flushcache && killall -HUP mDNSResponder'
+    alias flushdns="dscacheutil -flushcache && killall -HUP mDNSResponder"
     alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
-    alias r='open -a'
+    alias r="open -a"
     alias ydl="youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'"
-    alias nmap_localnet='nmap -sP 10.0.3.0/24'
-		alias fetch='curl -O'
-		alias htop='sudo htop'
-		alias sulast='sudo $(history -p !-1)'
+    alias nmap_localnet="nmap -sP 10.0.3.0/24"
+		alias fetch="curl -O"
+		alias htop="sudo htop"
+		alias sulast="sudo $(history -p !-1)"
   ;;
   Linux)
     # commands for Linux go here
@@ -35,6 +36,6 @@ case `uname` in
   FreeBSD)
     # commands for FreeBSD go here
     alias stl="sockstat -l"
-		alias sulast='doas $(history -p !-1)'
+		alias sulast="doas $(history -p !-1)"
   ;;
 esac
