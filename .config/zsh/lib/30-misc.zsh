321be93d14e0db4ef4c9f626ca50d4f055579040
Christian Baer <chris@debilux.org>
2020-10-11 22:15:10 +0200

    Move npm config to ~/.config

--- .config/zsh/lib/30-misc.zsh
+++ .config/zsh/lib/30-misc.zsh
@@ -13,13 +13,11 @@ export LC_CTYPE=$LANG
 export LC_ALL=de_DE.UTF-8
 export IOCAGE_COLOR=TRUE
 
-## npm config
-NPM_PACKAGES="${HOME}/.npm-packages"
-export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
-
 ## Set some app specific dirs with XDM scheme
 export VIMDOTDIR="$XDG_CONFIG_HOME/vim"
 export VIMINIT='let $VIMRC="$VIMDOTDIR/vimrc" | source $VIMRC'
 export GOPATH="${XDG_DATA_HOME}/go"
 export INPUTRC="${XDG_CONFIG_HOME}/readline/inputrc"
 export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
+export NPM_PACKAGES="${XDG_CONFIG_HOME}/npm/packages"
+export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
