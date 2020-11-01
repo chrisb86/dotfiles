2c14a499231cb03b041024900bb7867a5e550a5f
Christian Baer <chris@debilux.org>
2020-11-01 16:52:24 +0100

    Modified colors of ZSH prompt.

--- .config/zsh/lib/30-prompt.zsh
+++ .config/zsh/lib/30-prompt.zsh
@@ -7,11 +7,11 @@ if [[ "${SSH_CONNECTION}" ]]; then
 	p_ssh=" %F{green}⇣⇡%f";
 fi;
 
+p_user='%(!.%F{red}%B%n%b%f.%F{green}%n%f)'
 p_at='%(!.%F{red}%B#%b%f.%F{blue}@%f)'
-p_user='%(!.%F{red}%B%n%b%f.%F{blue}%n%f)'
 p_host='%F{blue}%m%f'
 p_path='%F{yellow}%~%f'
-p_pr='%(?.%F{blue}.%F{red}) 〉%f'
+p_pr='%(?.%F{white}.%F{red}) 〉%f'
 
 PS1="$p_user$p_at$p_host$p_ssh $p_path$p_pr"
-unset p_at p_user p_host p_path p_pr
+wunset p_at p_user p_host p_path p_pr
