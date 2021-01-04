6fb3fbfa7c481e0b0368d44aa9e6812f394e49d9
Christian Baer <chris@debilux.org>
2021-01-04 21:32:48 +0100

    Updated zsh git prompt

--- .config/zsh/lib/30-rprompt.zsh
+++ .config/zsh/lib/30-rprompt.zsh
@@ -7,8 +7,8 @@ autoload -U colors && colors # Enable colors in prompt
 
 # Modify the colors and symbols in these variables as desired.
 GIT_PROMPT_SYMBOL="%{$fg[blue]%}±"
-GIT_PROMPT_PREFIX="%{$fg[green]%}[%{$reset_color%}"
-GIT_PROMPT_SUFFIX="%{$fg[green]%}]%{$reset_color%}"
+GIT_PROMPT_PREFIX="%{$fg[green]%}(%{$reset_color%}"
+GIT_PROMPT_SUFFIX="%{$fg[green]%})%{$reset_color%}"
 GIT_PROMPT_AHEAD="%{$fg[red]%}ANUM%{$reset_color%}"
 GIT_PROMPT_BEHIND="%{$fg[cyan]%}BNUM%{$reset_color%}"
 GIT_PROMPT_MERGING="%{$fg[magenta]%}⚡︎%{$reset_color%}"
