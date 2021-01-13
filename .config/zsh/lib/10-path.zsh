ef97038e0893633bbe311367e024d86b8cdc29ac
Christian Baer <chris@debilux.org>
2021-01-13 23:29:53 +0100

    Modified zsh path

--- .config/zsh/lib/10-path.zsh
+++ .config/zsh/lib/10-path.zsh
@@ -7,15 +7,13 @@ _prepath() {
     for dir in "$@"; do
         dir=${dir:A}
         [[ ! -d "$dir" ]] && return
-        path=("$dir" $path[@])
+        #path=("$dir" $path[@])
+        path=("$dir" $path)
     done
 }
 
-_prepath /usr/bin /usr/sbin /sbin /Library/Apple/usr/bin  # macOS
-_prepath /usr/sbin /sbin # FreeBSD
-_prepath /usr/local/bin /bin /usr/local/sbin # General
-_prepath /opt/homebrew/bin /opt/homebrew/sbin # Homebrew an Apple Silicon
-_prepath ~/.local/bin
-_prepath ~/bin
+_prepath /usr/bin /bin /usr/sbin /sbin /usr/local/bin /usr/local/sbin # BSD and macOS
+_prepath /opt/homebrew/bin /opt/homebrew/sbin # Homebrew on Apple Silicon
+_prepath ~/bin ~/.local/bin # $HOME
 
 unfunction _prepath
