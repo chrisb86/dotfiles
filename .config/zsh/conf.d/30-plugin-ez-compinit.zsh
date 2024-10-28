4448cf65963708ea1ac8a028bf4c306258ac3bc0
Christian Baer <chris@debilux.org>
2024-10-28 18:32:28 +0100

    ZSH: Added config for ez-compinit

---
+++ .config/zsh/conf.d/30-plugin-ez-compinit.zsh
@@ -0,0 +1,1 @@
+# Available completion styles: gremlin, ohmy, prez, zshzoo# You can add your own too. To see all available completion styles# run 'compstyle -l'ZSH_COMPDUMP=${ZCACHE}/zcompdumpzstyle ':plugin:ez-compinit' 'compstyle' 'zshzoo'
