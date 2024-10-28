7014435399ca880b8e234b6cb4939f73df8ad3b7
Christian Baer <chris@debilux.org>
2024-10-28 22:28:56 +0100

    ZSH: Restructured path settings

--- .config/zsh/conf.d/10-path.zsh
+++ .config/zsh/conf.d/10-path.zsh
@@ -1,19 +1,18 @@
 # Set $PATH
 # Put only directories in $PATH that exist and remove duplicates
-typeset -U path  # No duplicates
+typeset -U path # No duplicates
 path=()
 
 _prepath() {
-    for dir in "$@"; do
-        dir=${dir:A}
-        [[ ! -d "$dir" ]] && return
-        #path=("$dir" $path[@])
-        path=("$dir" $path)
-    done
+for dir in "$@"; do
+  dir=${dir:A}
+  [[ ! -d "$dir" ]] && return
+  path=("$dir" $path)
+done
 }
 
 _prepath /usr/bin /bin /usr/sbin /sbin /usr/local/bin /usr/local/sbin # BSD and macOS
-_prepath /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin # Homebrew on macOS
-_prepath ~/bin ~/.local/bin # $HOME
+_prepath /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin          # Homebrew on macOS
+_prepath ~/bin ~/.local/bin                                           # $HOME
 
 unfunction _prepath
