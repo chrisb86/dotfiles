1d663f2a6005fc13d2e31b6c2d89a1a6a2c9922d
Christian Busch <chris@debilux.org>
2026-02-12 22:07:19 +0100

    ZSH: Updated path

--- .config/zsh/conf.d/10-path.zsh
+++ .config/zsh/conf.d/10-path.zsh
@@ -4,15 +4,19 @@ typeset -U path # No duplicates
 path=()
 
 _prepath() {
-for dir in "$@"; do
-  dir=${dir:A}
-  [[ ! -d "$dir" ]] && return
-  path=("$dir" $path)
-done
+  for dir in "$@"; do
+    dir=${dir:A}
+    [[ ! -d "$dir" ]] && continue
+    path=("$dir" $path)
+  done
 }
 
-_prepath /usr/bin /bin /usr/sbin /sbin /usr/local/bin /usr/local/sbin # BSD and macOS
-_prepath /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin          # Homebrew on macOS
-_prepath ~/bin ~/.local/bin                                           # $HOME
+_prepath /usr/bin /bin /usr/sbin /sbin /usr/local/bin /usr/local/sbin   # System
+_prepath /var/lib/flatpak/exports/bin                                    # Flatpak (Linux, system)
+_prepath /snap/bin                                                       # Snap (Linux)
+_prepath /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/sbin  # Homebrew (Linux)
+_prepath /opt/homebrew/bin /opt/homebrew/sbin                           # Homebrew (macOS)
+_prepath ~/.local/share/flatpak/exports/bin                               # Flatpak (Linux, user)
+_prepath ~/.local/bin ~/bin                                               # $HOME
 
 unfunction _prepath
